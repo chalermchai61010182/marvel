41a95d20df44c3eee091baca0880ab7269ed264e
chalermchai61010182 <61010182@kmitl.ac.th>
2021-11-05 13:41:48 +0700

    make it better

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+
+const liffId = "1656580161-nv4wjRBq";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(async () => {
+    const liff = (await import("@line/liff")).default;
+    try {
+      await liff.init({ liffId });
+    } catch (error) {
+      console.error("liff init error", error.message);
+    }
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  });
   return <Component {...pageProps} />;
 }
 
